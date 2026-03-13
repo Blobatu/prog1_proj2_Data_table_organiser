97c4e5dbc002f75bbd165ba60b6896d5439a85d1
Blobatu <antoinedesjardinsc@icloud.com>
2026-03-13 12:37:47 -0400

    learning to write to md

--- Data_table_organiser.py
+++ Data_table_organiser.py
@@ -7,14 +7,31 @@ import csv
 
 # fonctions
 
-def read_csv(file_path):
-    rows = []
-    with open(file_path, newline="") as f:
-        reader = csv.reader(f)
-        for row in reader:
-            rows.append(row)
-    return rows
+def read_csv(filename):
+    try:
+        rows = []
+        with open(filename, newline="") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                rows.append(row)
+        print(f"CSV file '{filename}' written sucessfully.")
+        return rows
+    except OSError as e:
+        print(f"Error reading file: {e}")
+
+def write_markdown_file(filename):
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write("| test | test | test |\n")
+            file.write("|:---: |:----:|:---: |\n")
+            file.write("| test | test | test |\n")
+            file.write("| test | test | test |\n")
+            file.write("| test | test | test |\n")
+        print(f"Markdown file '{filename}' written sucessfully.")
+    except OSError as e:
+        print(f"Error writing file: {e}")
+
 
 # lecture test
-data = read_csv("test_read.csv")
-print(data)
+read_csv("test_read.csv")
+write_markdown_file("test_table.md")
